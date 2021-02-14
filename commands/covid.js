9d0d7b6dc5a9e4c1ecef429cfa39621d958724c7
WscieklyDunczyk <pacz_to@pacz.to>
2021-02-14 19:58:41 +0100

    dodana walidacja komendy

--- commands/covid.js
+++ commands/covid.js
@@ -2,17 +2,26 @@ const { Message } = require('discord.js');
 const fetch = require('node-fetch');
 
 module.exports = async function(msg, args){
-    const url = `https://disease.sh/v3/covid-19/countries/${args}?yesterday=true&twoDaysAgo=false&strict=false&allowNull=false`;
-    fetch(url).then(res => res.json()).then(data => {if (data.msg) {
-		msg.reply(data.msg)
-		}
-        else {
-		msg.reply(`
-		Kraj: ${data.country}
-		Liczba śmierci: ${data.deaths}
-		Przypadki dzisiaj: ${data.todayCases}
-        Zgony dzisiaj: ${data.todayDeaths}
-		`)}
-    })
+	var country = args[0];
+	
+	if(args[0] === "polska"){
+		country = 'poland';
+	}
+	if(args[0] === undefined){
+		country = 'poland';
+	}
+	
+	const url = `https://disease.sh/v3/covid-19/countries/${country}?yesterday=true&twoDaysAgo=false&strict=false&allowNull=false`;
+		fetch(url).then(res => res.json()).then(data => {if (data.msg) {
+			msg.reply(data.msg)
+			}
+			else {
+			msg.reply(`
+			Kraj: ${data.country}
+			Liczba śmierci: ${data.deaths}
+			Przypadki dzisiaj: ${data.todayCases}
+			Zgony dzisiaj: ${data.todayDeaths}
+			`)}
+		})
 }
         
